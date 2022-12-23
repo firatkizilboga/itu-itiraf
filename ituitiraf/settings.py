414499c13ddc79cf2a26306b222b6a238087c346
itu-itis22-kizilboga20 <kizilboga20@itu.edu.tr>
2022-12-23 20:50:24 +0300

    fix to cors headers issue

--- ituitiraf/settings.py
+++ ituitiraf/settings.py
@@ -64,7 +64,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'https://firatkizilboga.pythonanywhere.com',
-    'http://firatkizilboga.pythonanywhere.com/',
+    'http://firatkizilboga.pythonanywhere.com',
 ]
 
 TEMPLATES = [
