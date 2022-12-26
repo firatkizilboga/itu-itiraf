f78b2ffc21913e79e4b6596dd20183464a1d280a
itu-itis22-kizilboga20 <kizilboga20@itu.edu.tr>
2022-12-26 15:08:58 +0300

    a fifth fix for the image bug

--- static/script.js
+++ static/script.js
@@ -14,8 +14,8 @@ async function getData() {
     var confBody = data[i].body;
     var confLikes = data[i].likes;
     var confComments = data[i].comments;
-    var confImg = data[i].img;
-
+    var confImg = data[i].image;
+    
     createConfessionDiv(confTitle, confDate, confBody, confImg, confLikes, confComments);
   }
   
