297eccdce861c5a5df2e58b966b04b5e4e86648f
firatkizilboga <firatkizilboga11@gmail.com>
2022-12-26 12:25:55 +0000

    settings.py edited

--- ituitiraf/urls.py
+++ ituitiraf/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path ('',include('confession.urls')),
     path ('api/',include('api.urls')),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
